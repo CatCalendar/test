72e8f81c119c0794a21b143a833140594620d214
NanaManBo <kimbosung22@naver.com>
2024-09-19 03:19:28 +0900

    다시 global.scss 코드 수정

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
 // pages/_app.tsx
 
-import '../styles/pages/globals.scss';
+import '../styles/global.scss'; // 글로벌 스타일 가져오기
 import { AppProps } from 'next/app';
 import { useEffect } from 'react';
 import { Provider } from 'react-redux';
